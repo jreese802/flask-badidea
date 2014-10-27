971687a90b65f90258b09a69a990e9c459383ea7
jreese802 <jreese@marlboro.edu>
2014-10-27 10:59:50 -0400

    Updated the flask main file

--- flasktest.py
+++ flasktest.py
@@ -1,13 +1,19 @@
 # web midterm?
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-  return '<h1>Hello World!</h1>'
+  return render_template('home.html')
+
+@app.route('/upload')
+def upload():
+  return render_template('upload.html')
 
 @app.route('/bootstrap/<name>')
 def bootstraptest(name):
