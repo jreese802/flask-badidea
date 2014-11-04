8823e7ef61d3bcb81c962e019dd376eb686a0c48
jreese802 <jreese@marlboro.edu>
2014-11-04 00:26:06 -0500

    Got most things sort of working.

--- db_config.py
+++ db_config.py
@@ -1,6 +1,6 @@
 import os
-from flask.ext.sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,8 +17,14 @@ db = SQLAlchemy(app)
 class Image(db.Model):
   __tablename__ = 'Image'
   image_id = db.Column(db.Integer, primary_key=True)
-  image_date = db.Column(db.Integer, index=True)
-  comments = db.relationship('Image', backref='Comment')
+  image_filename = db.Column(db.String, index=True)
+  image_date = db.Column(db.String, index=True)
+  comments = db.relationship('Comment', backref='comments')
+
+  def __init__(self, filename, date, comments=None):
+    self.image_filename = filename
+    self.image_date = date
+    self.image_comments = comments
 
   def __repr__(self):
     return '<Image {}>'.format(self.image_id)
@@ -26,9 +32,29 @@ class Image(db.Model):
 class Comment(db.Model):
   __tablename__ = 'Comment'
   comment_id = db.Column(db.Integer, primary_key=True)
-  content = db.Column(db.UnicodeText)
-  comment_date = db.Column(db.Integer, index=True)
+  comment_content = db.Column(db.UnicodeText)
+  comment_date = db.Column(db.String, index=True)
   image_id = db.Column(db.Integer, db.ForeignKey('Image.image_id'))  
 
-  def __repr(self):
+  def __init__(self, content, date, image_id):
+    self.comment_content = content
+    self.comment_date = date
+    self.image_id = image_id
+
+  def __repr__(self):
     return '<Comment {}>'.format(self.comment_id)
+
+# db.drop_all()
+# db.create_all()
+
+# image = Image('1234.jpg','1234')
+# image2 = Image('2345.png','2345')
+# image3 = Image('9765.jpg','9876')
+# db.session.add_all([image,image2,image3])
+
+# comment = Comment('This sucks.', '1235', 1)
+# comment2 = Comment('Totally.', '1236',1)
+# comment3 = Comment('Me IRL.', '1333', 3)
+# db.session.add_all([comment, comment2, comment3])
+
+# db.session.commit()
--- flasktest.py
+++ flasktest.py
@@ -5,6 +5,7 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import secure_filename
+import datetime
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -26,9 +27,14 @@ moment = Moment(app)
 class Image(db.Model):
   __tablename__ = 'Image'
   image_id = db.Column(db.Integer, primary_key=True)
-  image_name = db.Column(db.String, index=True, unique=True)
-  image_date = db.Column(db.Integer, index=True)
-  comments = db.relationship('Image', backref='comment', lazy='dynamic')
+  image_filename = db.Column(db.String, index=True)
+  image_date = db.Column(db.String, index=True)
+  comments = db.relationship('Comment', backref='comments')
+
+  def __init__(self, filename, date, comments=None):
+    self.image_filename = filename
+    self.image_date = date
+    self.image_comments = comments
 
   def __repr__(self):
     return '<Image {}>'.format(self.image_id)
@@ -36,46 +42,73 @@ class Image(db.Model):
 class Comment(db.Model):
   __tablename__ = 'Comment'
   comment_id = db.Column(db.Integer, primary_key=True)
-  content = db.Column(db.UnicodeText)
-  comment_date = db.Column(db.Integer, index=True)
+  comment_content = db.Column(db.UnicodeText)
+  comment_date = db.Column(db.String, index=True)
   image_id = db.Column(db.Integer, db.ForeignKey('Image.image_id'))  
 
-  def __repr(self):
+  def __init__(self, content, date, image_id):
+    self.comment_content = content
+    self.comment_date = date
+    self.image_id = image_id
+
+
+  def __repr__(self):
     return '<Comment {}>'.format(self.comment_id)
 
+db.create_all()
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/')
 def index():
   return render_template('home.html')
 
-@app.route('/uploads/')
-def upload_slash():
-  return redirect(url_for('upload_page'))
 
-@app.route('/upload', methods=['POST'])
-def upload():
-  file = request.files['file']
-  if file and allowed_file(file.filename):
-      filename = secure_filename(file.filename)
-      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-      return redirect(url_for('uploaded_file', filename=filename))
 
-@app.route('/uploads')
+
+@app.route('/upload')
 def upload_page():
   return render_template('upload.html')
 
-@app.route('/upload/<filename>')
+@app.route('/upload/')
+def upload_slash():
+  return redirect(url_for('upload_page'))
+
+@app.route('/images', methods=['POST'])
+def process_image():
+  file = request.files['file']
+  if file and allowed_file(file.filename):
+    filename = secure_filename(file.filename)
+    #make jpegs into jpgs
+    if file.filename[-4:] == 'jpeg':
+      file.filename = filename[:-4] + '.jpg'
+    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    image = Image(filename=filename,
+                  date=date,)
+    db.session.add(image)
+    db.session.commit()
+    return redirect(url_for('uploaded_file', filename=filename))
+  else:
+    render_template('error.html')
+
+@app.route('/images/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
-@app.route('/bootstrap/<name>')
-def bootstraptest(name):
-  return render_template('bootstraptest.html', name=name)
+@app.route('/browse')
+def browse():
+  image_list = db.session.query(Image).all()
+  return render_template('browse.html', image_list=image_list)
+
+@app.route('/browse/<page_number>')
+def browse_at_page(page_number):
+  image_list = db.session.query(Image).all()
+  return render_template('browse.html', image_list=image_list)
 
 @app.errorhandler(404)
 def page_not_found(e):
